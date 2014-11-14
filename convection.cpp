184158cdfbb18fdb3e5379f6870ef314e0ac9791
betterthangoogle <fabian.geier@aol.com>
2014-11-14 15:27:51 +0100

    convection almost done

--- convection.cpp
+++ convection.cpp
@@ -13,65 +13,83 @@ const double gravitationalConst=9.81; //m/s^2
 const double kappaConst=2./7.; 
 const double cpConst=1005;
 const double molarMassDryAir= 28.97; // g/mol
+const double z_scale=8000;
+
 
 double calculatePotTemp(double temp, double pressure);
 void print_vec(vector<double> vec);
 double calcDeltaTempSolarSurfaceHeating(double deltaTime,double eSolarSurface,double deltaPressure);
-void rt(int nlayer,int nmu, vector<double> opt_thick_vec,double LSurface,vector<double> sb_vec,vector<double>& Edelta );
+void radiativeTransfer(int nlayer,int nmu, vector<double> opt_thick_vec,double LSurface,vector<double> sb_vec,vector<double>& Edelta );
 double planck(double temp, double lambda_dn, double lambda_up );
+void planckVec(vector<double> tempVec, double lambda_dn, double lambda_up, vector <double> & bbVec);
 void getHeightfromPressure(vector<double> pressureVec, vector<double> heightVec);
-
-
+void calcOpticalThickFromHeight(double beta0,vector<double> he_vec,vector <double> &opt_thick_vec);
+void calcOpticalThickFromPressure(double beta0,vector<double> pressureVec,vector <double> &opt_thick_vec);
 int main(int argc, char** args){
 	
-	int nLayer;
+	int nLayer,nMu;
 	double e0=1362; // W/m^2
 	double albedo=0.3;
 	double eSolarSurface=e0/4 *(1-albedo);
 	double deltaTime=60; //in s
+	double beta0=0.0019;
 	vector<double> mid_pressure_vec;
-	vector<double> temp_vec;
+	vector<double> mid_temp_vec;
 	vector<double> height_vec;
-	//vector<double> 
+	vector<double> opt_thick_vec;
+	vector<double> plankRadiation_vec;
+	vector<double> Edelta;
 	double deltaPressure;
-	
-	if (argc==1){ 
-		cout<<"Try again -- Enter the number of layers you want to use."<<endl; 
+	double LSurface; 
+	if (argc!=3){ 
+		cout<<"Try again -- Usage: nLayer nMu"<<endl; 
 		return 0;
 	}	
 	else {
 		nLayer= atoi(args[1]);
+		nMu=atoi(args[2]);
 		}
 	
 	
 	
 	
-	mid_pressure_vec.resize(nLayer);
-	temp_vec.resize(nLayer);
-	fill(temp_vec.begin(),temp_vec.end(),0);
+	mid_pressure_vec.resize(nLayer-1);
+	mid_temp_vec.resize(nLayer-1);
+	opt_thick_vec.resize(nLayer-1);
+	plankRadiation_vec.resize(nLayer-1);
+
+
+	fill(mid_temp_vec.begin(),mid_temp_vec.end(),0);
 	
 	
 	deltaPressure=pressure0/(nLayer-1);
-	for(int i=0;i<nLayer-1;i++){
+	for(int i=0;i<mid_pressure_vec.size();i++){
 		mid_pressure_vec[i]=i*deltaPressure +deltaPressure/2.;
+		//cout<<mid_pressure_vec[i]<<endl;
 	}
 	
-
 	getHeightfromPressure(mid_pressure_vec, height_vec);
+		
+	calcOpticalThickFromPressure(beta0,mid_pressure_vec,opt_thick_vec);
 	
-	//print_vec(mid_pressure_vec);
-	//cout<<temp_vec[nLayer-1]<<endl;
-	
-	
-	//////LSurface berechnen!!////
-	//Lsurface=planck(288.15,lambda_dn,lambda_up)/M_PI; 
 
 
-	temp_vec[nLayer-1]+=calcDeltaTempSolarSurfaceHeating(deltaTime,eSolarSurface,deltaPressure);
+
+	mid_temp_vec.end()+=calcDeltaTempSolarSurfaceHeating(deltaTime,eSolarSurface,deltaPressure);
+	
+
+	//The surface Temp needs to be adapted. (At least I think so...) Maybe its temp_vec[nlayer-1] ?
+	LSurface=planck(288.15,12,100)/M_PI;
 	
 	
+	//Still have to create the temp_vec. 
+	//Probably a loop over the  lambda seqments.
+	planckVec(mid_temp_vec, 12, 100,plankRadiation_vec);
+	//planckVec(temp_vec, double lambda_dn, double lambda_up,plankRadiation_vec);
 	
-	//cout<<temp_vec[nLayer-1]<<endl;
+	radiativeTransfer(nLayer, nMu, opt_thick_vec,LSurface,plankRadiation_vec, Edelta );
+  	
+	
 	
 	
 	
@@ -86,17 +104,21 @@ void getHeightfromPressure(vector<double> pressureVec, vector<double> heightVec)
 	for(int i=0;i<pressureVec.size();i++){
 		heightVec[i]= -8000*log(pressureVec[i]/100000);  // 100.000 Pa Surface pressure
 		
-	if (heightVec[i]<1e-10) heightVec[i]=0;
-	//cout<<heightVec[i]<<"           "<< pressureVec[i]<<endl;
+	//if (heightVec[i]<1e-10) heightVec[i]=0;
+	//cout<<i<<"   "<<heightVec[i]<<"           "<< pressureVec[i]<<endl;
 	}
 
 
 }
 
-double calculatePotTemp(double temp, double pressure){
+double getPotTempFromTemp(double temp, double pressure){
 
 	return temp*pow(pressure/pressure0,kappaConst);
 
+}
+
+double getTempFromPotTemp(double potTemp, double pressure){
+	return potTemp*pow(pressure/pressure0,(-1)*kappaConst);
 }
 
 
@@ -111,28 +133,21 @@ double calcDeltaTempSolarSurfaceHeating(double deltaTime,double eSolarSurface,do
 
 
 void print_vec(vector<double> vec){
-  for(int i=0;i<vec.size();i++){
-    cout<<vec[i]<<endl;
-  }
-}
-void calcOpticalThick(double beta0,vector<double> he_vec,vector <double> &opt_thick_vec){
-	double z_scale=8000; //Skalenhoehe in m
-	//opt_thick_vec.resize(nlayer);
-	for(int i=0;i<opt_thick_vec.size();i++){
-	  opt_thick_vec[i]=beta0* exp(-(he_vec[i]+he_vec[i+1])/2/z_scale)*(he_vec[i]-he_vec[i+1]);
-	}
+  for(int i=0;i<vec.size();i++)	cout<<vec[i]<<endl;
 }
 
+void radiativeTransfer(int nlayer,int nmu, vector<double> opt_thick_vec,double LSurface,vector<double> sb_vec,vector<double>& Edelta ){
 
-void rt(int nlayer,int nmu, vector<double> opt_thick_vec,double LSurface,vector<double> sb_vec,vector<double>& Edelta ){
-  	
-   vector<double> Edn, Eup, Enet;
-   vector<double>L;
+  vector<double> Edn, Eup, Enet;
+  vector<double>L;
+  
   L.resize(nlayer+1);
   Edn.resize(nlayer+1);
   Eup.resize(nlayer+1);
   Enet.resize(nlayer+1);
   Edelta.resize(nlayer);
+  
+
   if(Edn.size()!=nlayer+1){
 	  Edn.resize(nlayer+1);
 	  Eup.resize(nlayer+1);
@@ -141,30 +156,26 @@ void rt(int nlayer,int nmu, vector<double> opt_thick_vec,double LSurface,vector<
 	}
  
 
-      for(double mu=1./nmu/2;mu<=1;mu+=1./nmu){
+    for(double mu=1./nmu/2;mu<=1;mu+=1./nmu){
     
-	L[0]=0;
-	for(int i=0;i<nlayer;i++){
-	  L[i+1]=L[i]*exp(-opt_thick_vec[i]/mu)+sb_vec[i]*(1-exp(-opt_thick_vec[i]/mu));
-	  Edn[i+1]=Edn[i+1]+L[i+1]*2*M_PI*mu*1./nmu;
-	}
-	fill(L.begin(),L.end(),sqrt(-1));
-	L[nlayer]= LSurface;//planck(288.15,lambda_dn,lambda_up)/M_PI;        //sigma*pow(288.15,4)/M_PI;
+		L[0]=0;
+		for(int i=0;i<nlayer;i++){
+		  L[i+1]=L[i]*exp(-opt_thick_vec[i]/mu)+sb_vec[i]*(1-exp(-opt_thick_vec[i]/mu));
+		  Edn[i+1]=Edn[i+1]+L[i+1]*2*M_PI*mu*1./nmu;
+		}
+		fill(L.begin(),L.end(),sqrt(-1));
+		L[nlayer]= LSurface;//planck(288.15,lambda_dn,lambda_up)/M_PI;        //sigma*pow(288.15,4)/M_PI;
+	    
+		Eup[nlayer]=Eup[nlayer]+L[nlayer]*2*M_PI*mu*1./nmu;
+	  
+		for(int i=nlayer;i>0;i--){
+		  L[i-1]=L[i]*exp(-opt_thick_vec[i-1]/mu)+sb_vec[i-1]*(1-exp(-opt_thick_vec[i-1]/mu));
+		  Eup[i-1]=Eup[i-1]+L[i-1]*2*M_PI*mu*1./nmu;
+		}
     
-	Eup[nlayer]=Eup[nlayer]+L[nlayer]*2*M_PI*mu*1./nmu;
-  
-	for(int i=nlayer;i>0;i--){
-	  L[i-1]=L[i]*exp(-opt_thick_vec[i-1]/mu)+sb_vec[i-1]*(1-exp(-opt_thick_vec[i-1]/mu));
-	  Eup[i-1]=Eup[i-1]+L[i-1]*2*M_PI*mu*1./nmu;
-	}
-    
-      }
+    }
 
-  //cout<<"Edn(TOA) " << Edn[0] <<"    Eup(TOA) "<< Eup[0] <<endl;
-  //cout<<"Edn(SURF)"<< Edn[nlayer] << "    Eup(SURF) "<< Eup[nlayer]<<endl;
-  // cout<<nmu<<"  "<<Eup[nlayer]<<endl;
-
-     for (int i=0;i<nlayer+1;i++){
+    for (int i=0;i<nlayer+1;i++){
       Enet[i]= Edn[i]-Eup[i];
 		}
 
@@ -175,24 +186,37 @@ void rt(int nlayer,int nmu, vector<double> opt_thick_vec,double LSurface,vector<
 }
 
 double planck(double temp, double lambda_dn, double lambda_up ){
-  double hcc=5.955214e-17;//Wm*2
-  double hc=1.986446e-25;//Jm
-  double k_b=1.38065e-23;// J/K
-  double sum=0;
-  double lambda;
- for(int i=lambda_dn;i<=lambda_up;i++){
-   lambda=i*1e-6;
-    sum+=2*hcc/pow(lambda,5)/(exp(hc/k_b/temp/lambda)-1)*1e-6;
-  }
-
- return M_PI* sum;
-  
+	double hcc=5.955214e-17;//Wm*2
+ 	double hc=1.986446e-25;//Jm
+  	double k_b=1.38065e-23;// J/K
+  	double sum=0;
+  	double lambda;
+ 	for(int i=lambda_dn;i<=lambda_up;i++){
+   		lambda=i*1e-6;
+    	sum+=2*hcc/pow(lambda,5)/(exp(hc/k_b/temp/lambda)-1)*1e-6;
+	}
+ 	return M_PI* sum; 
 }
 
 void planckVec(vector<double> tempVec, double lambda_dn, double lambda_up, vector <double> & bbVec){
   for (int i=0;i<tempVec.size();i++){
   	bbVec[i]=planck(tempVec[i],lambda_dn,lambda_up);
-
 	}
-  
+}
+
+void calcOpticalThickFromPressure(double beta0,vector<double> pressureVec,vector <double> &opt_thick_vec){
+	for(int i=1;i<opt_thick_vec.size();i++){
+			//opt_thick_vec[i]=beta0*pressure0/pow(pressureVec[i],2) *z_scale*(1+log(pressure0/pressureVec[i])) *(pressureVec[1]-pressureVec[0]);
+			opt_thick_vec[i]=   (-1)*beta0/pressure0 * z_scale * (pressureVec[1]-pressureVec[0]);
+			//opt_thick_vec[i]=beta0*z_scale/pressure0 *(pressureVec[i]*log(pressure0/pressureVec[i]) -pressureVec[i-1]*log(pressure0/pressureVec[i-1])); //(log(pressure0/pressureVec[i]))*(pressureVec[1]-pressureVec[0]);
+	}
+}
+
+
+void calcOpticalThickFromHeight(double beta0,vector<double> he_vec,vector <double> &opt_thick_vec){
+	//Skalenhoehe in m
+	//opt_thick_vec.resize(nlayer);
+	for(int i=0;i<opt_thick_vec.size();i++){
+	  opt_thick_vec[i]=beta0* exp(-(he_vec[i]/z_scale))*(he_vec[i]-he_vec[i+1]);
+	}
 }
